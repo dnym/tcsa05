4fe472baa2b20388099a68d487f97748025aab83
David Nyman <david.nyman@protonmail.com>
2023-10-20 10:45:08 +0200

    Add lists for models

--- Flashcards/Program.cs
+++ Flashcards/Program.cs
@@ -1,7 +1,13 @@
-﻿namespace Flashcards;
+﻿using Flashcards.Models;
+
+namespace Flashcards;
 
 internal static class Program
 {
+    public static readonly List<Stack> Stacks = new();
+    public static readonly List<Flashcard> Flashcards = new();
+    public static readonly List<StudySession> StudySessions = new();
+
     static void Main()
     {
         var screen = UI.MainMenu.Get();
