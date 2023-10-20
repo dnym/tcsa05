45f993e49f34dd14ce9784e5b220359f91115b5f
David Nyman <david.nyman@protonmail.com>
2023-10-20 15:14:07 +0200

    Improve stack management menu

--- Flashcards/UI/ManageStacksMenu.cs
+++ Flashcards/UI/ManageStacksMenu.cs
@@ -9,18 +9,26 @@ internal static class ManageStacksMenu
 {
     public static Screen Get()
     {
-        const int headerHeight = 1 + Screen.HeaderSeparatorHeight + Screen.HeaderPadding;
+        const int headerHeight = 1;
         // Actual footer height varies, but using a constant simplifies things (including for the user).
         const int footerHeight = Screen.FooterPadding + Screen.FooterSeparatorHeight + 4;
+        const string promptText = "\nSelect a Stack: ";
         const int promptHeight = 2;
         PaginationResult? paginationResult = null;
+        int previouslyUsableHeight = -1;
         int skip = 0;
 
         Screen screen = new(header: (_, usableHeight) =>
         {
+            if (usableHeight != previouslyUsableHeight)
+            {
+                // Reset pagination when the window size changes.
+                previouslyUsableHeight = usableHeight;
+                skip = 0;
+            }
             int heightAvailableToBody = usableHeight - (headerHeight + footerHeight);
             paginationResult = DeterminePagination(heightAvailableToBody, Program.Stacks.Count, perPageListHeightOverhead: promptHeight, skippedItems: skip);
-            if (paginationResult.TotalPages > 0)
+            if (paginationResult.TotalPages > 1)
             {
                 return $"Manage Stacks (page {paginationResult.CurrentPage}/{paginationResult.TotalPages})";
             }
@@ -33,7 +41,13 @@ internal static class ManageStacksMenu
             if (paginationResult!.TotalPages > 0)
             {
                 var take = paginationResult.ItemsPerPage;
-                return GetStackList(skip, take);
+                return GetStackList(skip, take) + promptText;
+            }
+            else if (Program.Stacks.Any() && paginationResult!.TotalPages == 0)
+            {
+                // Note that this may actually not be true due to reserving space for PageUp and PageDown hints.
+                // TODO: Consider whether always printing the PageUp and PageDown hints, to not annoy the user by refusing to print items when there is space.
+                return "Window is too small to list any stacks.";
             }
             else
             {
@@ -41,14 +55,14 @@ internal static class ManageStacksMenu
             }
         }, footer: (_, _) =>
         {
-            var footerText = "Press [N] to create a new stack,\n";
+            var footerText = "Press [F1] to create a new stack,\n";
             if (paginationResult!.CurrentPage > 1)
             {
-                footerText += " [PgUp] to go to the previous page,\n";
+                footerText += "[PgUp] to go to the previous page,\n";
             }
             if (paginationResult.CurrentPage < paginationResult.TotalPages)
             {
-                footerText += " [PgDown] to go to the next page,\n";
+                footerText += "[PgDown] to go to the next page,\n";
             }
             footerText += "or [Esc] to go back.";
             return footerText;
@@ -58,9 +72,9 @@ internal static class ManageStacksMenu
         {
         }
 
-        screen.AddAction(ConsoleKey.N, () =>
+        screen.AddAction(ConsoleKey.F1, () =>
         {
-            Console.WriteLine("CreateStackMenu.Get().Show()");
+            CreateStackMenu.Get().Show();
 
             if (Program.Stacks.Count > 0)
             {
