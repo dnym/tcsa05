92c6198eefed154dc3df34fdd7c365a77c2cdde5
David Nyman <david.nyman@protonmail.com>
2023-10-20 10:44:10 +0200

    Add pagination helper

--- TCSAHelper/Console/Utils.cs
+++ TCSAHelper/Console/Utils.cs
@@ -1,6 +1,33 @@
 ﻿namespace TCSAHelper.Console;
-internal static class Utils
+public static class Utils
 {
+    public class PaginationResult
+    {
+        public int ItemsPerPage { get; set; }
+        public int CurrentPage { get; set; }
+        public int TotalPages { get; set; }
+    }
+
+    public static PaginationResult DeterminePagination(int heightAvailableToList, int listLength, int heightPerItem = 1, int perPageListHeightOverhead = 0, int skippedItems = 0)
+    {
+        if (heightAvailableToList < 1) throw new ArgumentException("Height available to list must be a positive number.");
+        if (listLength < 0) throw new ArgumentException("List length must be a positive number.");
+        if (heightPerItem < 1) throw new ArgumentException("Height per item must be a positive number.");
+        if (perPageListHeightOverhead < 0) throw new ArgumentException("Per page list height overhead must be a positive number.");
+        if (skippedItems < 0) throw new ArgumentException("Skipped items must be a positive number.");
+
+        int itemsPerPage = (heightAvailableToList - perPageListHeightOverhead) / heightPerItem;
+        int totalPages = (int)Math.Ceiling((double)listLength / itemsPerPage);
+        int currentPage = (int)Math.Ceiling((double)skippedItems / itemsPerPage) + 1;
+
+        return new PaginationResult
+        {
+            ItemsPerPage = itemsPerPage,
+            CurrentPage = currentPage,
+            TotalPages = totalPages
+        };
+    }
+
     internal static void ClearRestOfLine(char c = ' ')
     {
         var currentLine = System.Console.CursorTop;
