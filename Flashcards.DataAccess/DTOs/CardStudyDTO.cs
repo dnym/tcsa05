13c09f839cb51dc5fcb51cf590b9c9aa298f8906
David Nyman <david.nyman@protonmail.com>
2023-10-24 08:07:57 +0200

    Add DTOs

---
+++ Flashcards.DataAccess/DTOs/CardStudyDTO.cs
@@ -0,0 +1,8 @@
+﻿namespace Flashcards.DataAccess.DTOs;
+
+public class CardStudyDTO
+{
+    public int FlashcardId { get; set; }
+    public bool WasCorrect { get; set; }
+    public DateTime AnsweredAt { get; set; }
+}
