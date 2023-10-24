6450976fd2a362f387cc2dd2c691855c8624bde6
David Nyman <david.nyman@protonmail.com>
2023-10-24 09:15:25 +0200

    Add DB interface

---
+++ Flashcards.DataAccess/IDataAccess.cs
@@ -0,0 +1,14 @@
+﻿using Flashcards.DataAccess.DTOs;
+
+namespace Flashcards.DataAccess;
+
+public interface IDataAccess
+{
+    Task<List<StackListItem>> GetStackListAsync(int take, int skip = 0);
+    Task CreateStackAsync(NewStack stack);
+    Task<List<ExistingFlashcard>> GetFlashcardListAsync(int stackId);
+    Task CreateFlashcardAsync(NewFlashcard flashcard);
+    Task UpdateFlashcardAsync(ExistingFlashcard flashcard);
+    Task<List<HistoryListItem>> GetHistoryListAsync();
+    Task AddHistoryAsync(NewHistory history);
+}
