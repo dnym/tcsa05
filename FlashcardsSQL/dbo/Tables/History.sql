c65960d3274d81bfea1fe4fd01ae0a34b0b3800f
David Nyman <david.nyman@protonmail.com>
2023-11-09 15:29:13 +0100

    Change cascade to trigger

--- FlashcardsSQL/dbo/Tables/History.sql
+++ FlashcardsSQL/dbo/Tables/History.sql
@@ -5,3 +5,15 @@
     [StartedAt] DATETIME2 NOT NULL, 
     CONSTRAINT [History_PertainsTo_Stack_fk] FOREIGN KEY ([StackId]) REFERENCES [Stack]([StackId]) ON DELETE CASCADE
 )
+
+GO
+
+CREATE TRIGGER [dbo].[Delete_StudyResults_trg]
+    ON [dbo].[History]
+    FOR DELETE
+    AS
+    BEGIN
+        SET NOCOUNT ON;
+        DELETE FROM [dbo].[StudyResult]
+		WHERE [HistoryId] IN (SELECT [HistoryId] FROM DELETED);
+    END
--- FlashcardsSQL/dbo/Tables/StudyResult.sql
+++ FlashcardsSQL/dbo/Tables/StudyResult.sql
@@ -5,6 +5,7 @@
     [FlashcardId] INT NOT NULL, 
     [WasCorrect] BIT NOT NULL, 
     [AnsweredAt] DATETIME2 NOT NULL, 
-    CONSTRAINT [StudyResult_BelongsTo_History_fk] FOREIGN KEY ([HistoryId]) REFERENCES [History]([HistoryId]) ON DELETE CASCADE, 
+    --Constraint changed to trigger in History.sql
+    --CONSTRAINT [StudyResult_BelongsTo_History_fk] FOREIGN KEY ([HistoryId]) REFERENCES [History]([HistoryId]) ON DELETE CASCADE, 
     CONSTRAINT [StudyResult_PertainsTo_Flashcard_fk] FOREIGN KEY ([FlashcardId]) REFERENCES [Flashcard]([FlashcardId]) ON DELETE CASCADE
 )
