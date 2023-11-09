6451ff1c6c8820cc6e38d4a08fe89bcb3fcb58e6
David Nyman <david.nyman@protonmail.com>
2023-11-09 14:20:57 +0100

    Add tables

---
+++ FlashcardsSQL/dbo/Tables/History.sql
@@ -0,0 +1,7 @@
+﻿CREATE TABLE [dbo].[History]
+(
+	[HistoryId] INT NOT NULL PRIMARY KEY IDENTITY, 
+    [StackId] INT NOT NULL, 
+    [StartedAt] DATETIME2 NOT NULL, 
+    CONSTRAINT [History_PertainsTo_Stack_fk] FOREIGN KEY ([StackId]) REFERENCES [Stack]([StackId]) ON DELETE CASCADE
+)
