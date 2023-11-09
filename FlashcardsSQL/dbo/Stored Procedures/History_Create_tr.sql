435a347ee2bc09ba38a85513e4cd2ebdaf6cc98f
David Nyman <david.nyman@protonmail.com>
2023-11-09 14:42:53 +0100

    Add creation SPs

---
+++ FlashcardsSQL/dbo/Stored Procedures/History_Create_tr.sql
@@ -0,0 +1,10 @@
+﻿CREATE PROCEDURE [dbo].[History_Create_tr]
+	@StartedAt datetime2(7),
+	@StackId int
+AS
+	BEGIN
+		SET NOCOUNT ON;
+		INSERT INTO [dbo].[History] ([StartedAt], [StackId])
+		VALUES (@StartedAt, @StackId);
+	END
+RETURN 0
---
+++ FlashcardsSQL/dbo/Stored Procedures/StudyResult_Create_tr.sql
@@ -0,0 +1,11 @@
+﻿CREATE PROCEDURE [dbo].[StudyResult_Create_tr]
+	@FlashcardId int,
+	@WasCorrect bit,
+	@AnsweredAt datetime2(7)
+AS
+	BEGIN
+		SET NOCOUNT ON;
+		INSERT INTO [dbo].[StudyResult] ([FlashcardId], [WasCorrect], [AnsweredAt])
+		VALUES (@FlashcardId, @WasCorrect, @AnsweredAt);
+	END
+RETURN 0
