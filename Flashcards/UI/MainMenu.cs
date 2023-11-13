15d1f6065d241fbc1b791e87876d6e1841d29ff9
David Nyman <david.nyman@protonmail.com>
2023-11-13 12:03:19 +0100

    Add keypad support

--- Flashcards/UI/MainMenu.cs
+++ Flashcards/UI/MainMenu.cs
@@ -22,12 +22,16 @@ internal static class MainMenu
 
         screen.AddAction(ConsoleKey.S, StudySession);
         screen.AddAction(ConsoleKey.D1, StudySession);
+        screen.AddAction(ConsoleKey.NumPad1, StudySession);
         screen.AddAction(ConsoleKey.M, ManageStacks);
         screen.AddAction(ConsoleKey.D2, ManageStacks);
+        screen.AddAction(ConsoleKey.NumPad2, ManageStacks);
         screen.AddAction(ConsoleKey.H, StudyHistory);
         screen.AddAction(ConsoleKey.D3, StudyHistory);
+        screen.AddAction(ConsoleKey.NumPad3, StudyHistory);
         screen.AddAction(ConsoleKey.Q, screen.ExitScreen);
         screen.AddAction(ConsoleKey.D0, screen.ExitScreen);
+        screen.AddAction(ConsoleKey.NumPad0, screen.ExitScreen);
 
         return screen;
     }
--- Flashcards/UI/ManageSingleStackMenu.cs
+++ Flashcards/UI/ManageSingleStackMenu.cs
@@ -18,9 +18,11 @@ internal static class ManageSingleStackMenu
             footer: (_, _) => "Select by pressing a number or letter,\nor press [Esc] to go back.");
         screen.AddAction(ConsoleKey.C, () => CreateOrEditFlashcard.Get(dataAccess, stackId).Show());
         screen.AddAction(ConsoleKey.D1, () => CreateOrEditFlashcard.Get(dataAccess, stackId).Show());
+        screen.AddAction(ConsoleKey.NumPad1, () => CreateOrEditFlashcard.Get(dataAccess, stackId).Show());
 
         screen.AddAction(ConsoleKey.B, () => ManageFlashcardsMenu.Get(dataAccess, stack.Id).Show());
         screen.AddAction(ConsoleKey.D2, () => ManageFlashcardsMenu.Get(dataAccess, stack.Id).Show());
+        screen.AddAction(ConsoleKey.NumPad2, () => ManageFlashcardsMenu.Get(dataAccess, stack.Id).Show());
 
         void CreateOrRenameStack()
         {
@@ -29,6 +31,7 @@ internal static class ManageSingleStackMenu
         }
         screen.AddAction(ConsoleKey.R, CreateOrRenameStack);
         screen.AddAction(ConsoleKey.D3, CreateOrRenameStack);
+        screen.AddAction(ConsoleKey.NumPad3, CreateOrRenameStack);
 
         void DeleteStack()
         {
@@ -37,10 +40,13 @@ internal static class ManageSingleStackMenu
         }
         screen.AddAction(ConsoleKey.D, DeleteStack);
         screen.AddAction(ConsoleKey.D4, DeleteStack);
+        screen.AddAction(ConsoleKey.NumPad4, DeleteStack);
 
         screen.AddAction(ConsoleKey.S, screen.ExitScreen);
         screen.AddAction(ConsoleKey.D0, screen.ExitScreen);
         screen.AddAction(ConsoleKey.Escape, screen.ExitScreen);
+        screen.AddAction(ConsoleKey.NumPad0, screen.ExitScreen);
+
         return screen;
     }
 }
